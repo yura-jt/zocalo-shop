2b988052d0578cd385e0bbe2d7fa73badd75bd9f
yura_jt <cryobyte.zero@gmail.com>
2020-05-14 08:32:37 +0300

    add repositories

---
+++ src/main/java/com/zocalo/shop/repository/CartRepository.java
@@ -0,0 +1,9 @@
+package com.zocalo.shop.repository;
+
+import com.zocalo.shop.entity.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart, Integer> {
+}
